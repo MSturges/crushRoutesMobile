3109bbfd063abbf3cb095b2f5be2a5b6a17a03fb
MSturges <Sturges3232@gmail.com>
2016-08-30 13:25:32 -0600

    foundation of ionic app

---
+++ www/js/controllers/controllers.js
@@ -0,0 +1,41 @@
+angular.module('app')
+  
+.controller('addRouteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('routeListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('mapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
---
+++ www/js/routes/routes.js
@@ -0,0 +1,65 @@
+angular.module('app')
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+  .state('tab.addRoute', {
+    url: '/addRoute',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/addRoute.html',
+        controller: 'addRouteCtrl'
+      }
+    }
+  })
+
+  .state('tab.routeList', {
+    url: '/routeList',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/routeList.html',
+        controller: 'routeListCtrl'
+      }
+    }
+  })
+
+  .state('tab.map', {
+    url: '/Map',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/map.html',
+        controller: 'mapCtrl'
+      }
+    }
+  })
+
+  .state('tab', {
+    url: '/tab',
+    templateUrl: 'templates/tab.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  $urlRouterProvider.otherwise('/tab/addRoute')
+
+
+
+});
