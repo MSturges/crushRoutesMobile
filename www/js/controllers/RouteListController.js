1d51626ba9472518f3883c05b2eeb38dad3c92d2
MSturges <Sturges3232@gmail.com>
2016-09-15 10:34:48 -0600

    local storage works on ionic serve...testing on ionic view now

--- www/js/controllers/RouteListController.js
+++ www/js/controllers/RouteListController.js
@@ -1,15 +1,22 @@
 angular.module('app')
 
-.controller('routeListCtrl', ['$scope', '$stateParams', 'GrabClimbingArea', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-  // You can include any angular dependencies as parameters for this function
-  // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function($scope, $stateParams, GrabClimbingArea) {
-    GrabClimbingArea.grabAreas()
-      .then(function(res) {
-        $scope.climbingAreas = res.data;
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
-  }
+.controller('routeListCtrl', ['$scope', '$stateParams', 'GrabClimbingArea', '$window', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+
+function($scope, $stateParams, GrabClimbingArea, $window) {
+
+  GrabClimbingArea.grabAreas()
+  .then(function(res) {
+    $scope.climbingAreas = res.data;
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+
+  $scope.test = $window.localStorage.getItem('token');
+  $scope.test2 = $window.localStorage.getItem('user');
+
+
+}
 ])
