c0f1a7a606493cea315b22e707767822479b4fad
MSturges <Sturges3232@gmail.com>
2016-09-15 09:27:39 -0600

    injected ngMessages into module

--- www/js/module/app.js
+++ www/js/module/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'ngCordova'])
+angular.module('app', ['ionic', 'ngCordova', 'ngMessages'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
